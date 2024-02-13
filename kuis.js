d772a450c2ce36578d9c0bcc068938c086f6ccc6
dapraws <darrelprawira26@gmail.com>
2024-02-13 17:39:17 +0700

    functional programming

--- kuis.js
+++ kuis.js
@@ -1,63 +1,34 @@
 /**
  * TODO:
- * 1. Buatlah class bernama Animal dengan ketentuan:
- *    - Memiliki properti:
- *      - name: string
- *      - age: int
- *      - isMammal: boolean
- *    - Memiliki constructor untuk menginisialisasi properti:
- *      - name
- *      - age
- *      - isMammal
- * 2. Buatlah class bernama Rabbit dengan ketentuan:
- *    - Merupakan turunan dari class Animal
- *    - Memiliki method:
- *      - eat yang mengembalikan nilai string `${this.name} sedang makan!`
- *    - Ketika diinstansiasi, properti isMammal harus bernilai true
- * 3. Buatlah class bernama Eagle dengan ketentuan:
- *    - Merupakan turunan dari class Animal
- *    - Memiliki method:
- *      - fly yang mengembalikan nilai string `${this.name} sedang terbang!`
- *    - Ketika diinstansiasi, properti isMammal harus bernilai false
- * 4. Buatlah instance dari class Rabbit bernama "myRabbit" dengan ketentuan:
- *    - properti name bernilai: "Labi"
- *    - properti age bernilai: 2
- * 5. Buatlah instance dari class Eagle bernama "myEagle" dengan ketentuan:
- *    - properti name bernilai: "Elo"
- *    - properti age bernilai: 4
+ * Buatlah variabel greatAuthors yang merupakan array
+ * berdasarkan hasil filter() dan map() dari books:
+ *   - Gunakan fungsi filter untuk mengembalikan nilai item books
+ *     yang hanya memiliki nilai sales lebih dari 1000000.
+ *   - Gunakan fungsi map pada books yang sudah ter-filter,
+ *     untuk mengembalikan nilai string dengan format:
+ *     - `${author} adalah penulis buku ${title} yang sangat hebat!`
+ *
+ * Catatan: Jangan ubah nilai atau struktur dari books
  */
 
+const books = [
+  { title: "The Da Vinci Code", author: "Dan Brown", sales: 5094805 },
+  { title: "The Ghost", author: "Robert Harris", sales: 807311 },
+  { title: "White Teeth", author: "Zadie Smith", sales: 815586 },
+  { title: "Fifty Shades of Grey", author: "E. L. James", sales: 3758936 },
+  { title: "Jamie's Italy", author: "Jamie Oliver", sales: 906968 },
+  { title: "I Can Make You Thin", author: "Paul McKenna", sales: 905086 },
+  {
+    title: "Harry Potter and the Deathly Hallows",
+    author: "J.K Rowling",
+    sales: 4475152,
+  },
+];
+
 // Tulis kode di bawah ini
-class Animal {
-  constructor(name, age, isMammal) {
-    this.name = name;
-    this.age = age;
-    this.isMammal = isMammal;
-  }
-}
-
-class Rabbit extends Animal {
-  constructor(name, age) {
-    super(name, age, true);
-  }
-
-  eat() {
-    return `${this.name} sedang makan!`;
-  }
-}
-
-class Eagle extends Animal {
-  constructor(name, age) {
-    super(name, age, false);
-  }
-
-  fly() {
-    return `${this.name} sedang terbang!`;
-  }
-}
-
-// Membuat instance Rabbit
-const myRabbit = new Rabbit("Labi", 2);
-
-// Membuat instance Eagle
-const myEagle = new Eagle("Elo", 4);
+const greatAuthors = books
+  .filter((book) => book.sales > 1000000)
+  .map(
+    (book) =>
+      `${book.author} adalah penulis buku ${book.title} yang sangat hebat!`
+  );
